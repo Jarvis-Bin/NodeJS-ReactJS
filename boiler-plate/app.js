33f58e16b0f757e76dbdd128700d9f3b4914a72b
Jarvis-Bin <wjdqlsdlekt1@gmail.com>
2022-05-16 23:57:41 +0900

    app 수정

--- boiler-plate/app.js
+++ boiler-plate/app.js
@@ -3,6 +3,6 @@ const models = require("./models/index");
 models.sequelize.sync().then(   () => {
     console.log("DB 연결 성공");
 }).catch(err => {
-    console.log("연결 실패");
+    console.log("연결 실패ㅜㅠ");
     console.log(err);
 })
