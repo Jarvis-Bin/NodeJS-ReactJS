5bf85f130869bc840bee81c8422537d5c839c63c
Jarvis-Bin <wjdqlsdlekt1@gmail.com>
2022-05-25 19:34:44 +0900

    mongodb연결

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jarvis-bin:6718520z@boilerplate.qihyr.mongodb.net/?retryWrites=true&w=majority', {
+    // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World! jarvis')
 })
